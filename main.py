c8a85699da1af6772d28aba00cd458c8df95574f
Matthew T <65698531+MJTFreeTime@users.noreply.github.com>
2021-07-20 21:16:09 +0000

    V1.01 Update || New  command and character pictures to  command!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 import requests
 import json
+from googleapiclient.discovery import build
+import random
 
 ############# MYANIMELIST API #############
 CLIENT_ID = os.environ['CLIENT_ID']
@@ -35,6 +37,10 @@ def animeRankings(access_token: str):
 
     return rankingsData
 
+######## GOOGLE API #########
+google_key = os.environ['GOOGLE_KEY']
+search_engine_id = os.environ['SEARCH_ENGINE_ID']
+
 ############# DISCORD #############
 
 client = discord.Client()
@@ -42,12 +48,12 @@ client = discord.Client()
 def getQuote():
     response = requests.get("https://animechan.vercel.app/api/random")
     json_data = json.loads(response.text)
-    quote = "\"" + json_data["quote"] + "\"" + " - " + json_data["character"] + " from " + json_data["anime"]
+    quote = [json_data["quote"], json_data["character"], json_data["anime"]]
     return quote
 
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+    print("Successfully logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
@@ -58,7 +64,25 @@ async def on_message(message):
         await message.channel.send("Kon'nichiwa")
 
     if message.content.startswith("$quote"):
-        quote = getQuote()
+        quoteData = getQuote()
+        quote = "\"" + quoteData[0] + "\"" + " - " + quoteData[1] + " from " + quoteData[2]
+
+        ### Picture ###
+        search = quoteData[1] + " from " + quoteData[2]
+        ran = random.randint(0, 9)
+        resource = build("customsearch", "v1", developerKey = google_key).cse()
+        result = resource.list(
+            q=f"{search}", cx=search_engine_id, searchType="image", safe="active"
+        ).execute()
+ 
+        try:
+            url = result["items"][ran]["link"]
+            embed1 = discord.Embed(title=f"Image of {search}")
+            embed1.set_image(url=url)
+            await message.channel.send(embed=embed1)
+        except:
+            print(f"Image of {search} cannot be sent. (Explicit Content?)")
+
         await message.channel.send(quote)
 
     if message.content.startswith("$animerankings"):
@@ -68,6 +92,14 @@ async def on_message(message):
         for anime in rankings['data']:
             toSend += '\n#' + str(anime['ranking']['rank']) + ': ' + anime['node']['title']
         
+        await message.channel.send(toSend)
+
+    if message.content.startswith("$help"):
+        toSend = """:robot: Current commands:
+\$help -- Get a list of working commands
+\$hello  -- Say hello to the bot! What will they reply?
+\$quote -- Let the bot tell you a random anime quote!
+\$animerankings -- List of current top 10 most popular anime"""
         await message.channel.send(toSend)
 
 client.run(os.environ['TOKEN'])
